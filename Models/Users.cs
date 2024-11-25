ea6641e85761c44b647653bc476febfc33d5b935
Recep Bekir Gurbuz <recepbekirgrbz9@gmail.com>
2024-11-25 22:22:37 +0300

    I created an mvc c# asp.net entity framework project from visual studio

---
+++ Models/Users.cs
@@ -0,0 +1,11 @@
+﻿
+namespace asp_net_restful_api.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+    }
+}
