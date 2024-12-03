55098d5505b499b2dcaee145dedcd64285da664e
Recep Bekir Gurbuz <recepbekirgrbz9@gmail.com>
2024-12-03 22:59:45 +0300

    The database connection has been made and when the login address is called with get from the http method, the login.cshtml file in the account folder that we created in the views folder will be displayed on the screen.

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using asp_net_restful_api.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace asp_net_restful_api.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public AccountController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("/login")]
+        public IActionResult Login()
         {
             return View();
         }
+
     }
 }
